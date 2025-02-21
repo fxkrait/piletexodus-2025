523835bbe1326b56c85633bc7b07d5f7acf08d37
FXKrait <thisisvoid@disroot.org>
2025-02-20 17:54:39 -0800

    update via gp

---
+++ src/termuxShortcuts/creativity.static.sh
@@ -0,0 +1,2 @@
+sh "/data/data/com.termux/files/home/piletexodus-2025/src/utils/picturegrid/2025-renameAndMovePhoto.sh"
+sh "/data/data/com.termux/files/home/piletexodus-2025/src/utils/picturegrid/2025-moveStaticHolds.sh"
