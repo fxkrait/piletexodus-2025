cc44d9eedf80f823e9c0e5a44461c6db60838105
FXKrait <thisisvoid@disroot.org>
2025-02-20 18:12:51 -0800

    update via gp

--- src/termuxShortcuts/creativity.static.sh
+++ src/termuxShortcuts/creativity.static.sh
@@ -1,2 +1,3 @@
 sh "/data/data/com.termux/files/home/piletexodus-2025/src/utils/picturegrid/2025-renameAndMovePhoto.sh"
 sh "/data/data/com.termux/files/home/piletexodus-2025/src/utils/picturegrid/2025-moveStaticHolds.sh"
+sh "/data/data/com.termux/files/home/piletexodus-2025/src/utils/picturegrid/2025-gitPullAndPush.sh"
---
+++ src/utils/picturegrid/2025-gitPullAndPush.sh
@@ -0,0 +1,3 @@
+cd "/data/data/com.termux/files/home/piletexodus-2025/"
+git pull
+git add . && git commit -m 'update via gp' && git push
