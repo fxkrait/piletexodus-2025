1bbfd4bfc276b74598b85d96528e429949619601
FXKrait <thisisvoid@disroot.org>
2025-02-20 17:51:27 -0800

    update via gp

---
+++ src/utils/picturegrid/2025-moveStaticHolds.sh
@@ -0,0 +1,3 @@
+inputFileName="/data/data/com.termux/files/home/storage/dcim/piletexodus/$(date +%F).jpg"
+outputFileName="/data/data/com.termux/files/home/piletexodus-2025/feeds/creativity/staticHolds/files/$(date +%F).jpg"
+mv $inputFileName $outputFileName
